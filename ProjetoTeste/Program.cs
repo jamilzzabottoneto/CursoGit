83809bb4fe7312541bed1936ac6883c616208012
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-03-01 11:52:55 -0300

    Adicionando uma estrutura de repetição

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -10,6 +10,11 @@ namespace ProjetoTeste
             Console.WriteLine("Bom Dia");
             Console.WriteLine("Boa Tarde");
             Console.WriteLine("Boa Noite");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
